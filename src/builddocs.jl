e1bbc6cfd53257118e771ad17c838d5ab333a0b9
David Anthoff <anthoff@berkeley.edu>
2015-07-02 16:00:55 -0700

    Change location of auto generated documentation files

--- src/builddocs.jl
+++ src/builddocs.jl
@@ -2,7 +2,7 @@ using Lexicon
 using IAMF
 
 save(
-	normpath(Pkg.dir("IAMF"), "doc", "generated", "reference.md"),
+	normpath(Pkg.dir("IAMF"), "doc", "reference.md"),
 	IAMF,
 	include_internal=false,
 	md_subheader=:skip)
